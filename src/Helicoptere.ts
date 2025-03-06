0d7d31a4796277a65746277ba50470f8c53ef4a8
aLanois14 <axel.lanois@outlook.fr>
2025-03-06 14:47:20 +0100

    init project

---
+++ src/Helicoptere.ts
@@ -0,0 +1,29 @@
+import { Direction } from "./Direction";
+import { ModuleMartien } from "./ModuleMartien";
+import { Position } from "./Position";
+import { Rover } from "./Rover";
+
+export class Helicoptere extends Rover implements ModuleMartien {
+  constructor(
+    override readonly direction: Direction,
+    override readonly position: Position
+  ) {
+    super(direction, position);
+  }
+
+  override monter() {
+    this._position = new Position(
+      this._position.x,
+      this._position.y,
+      this._position.z + 1
+    );
+  }
+
+  override descendre() {
+    this._position = new Position(
+      this._position.x,
+      this._position.y,
+      this._position.z - 1
+    );
+  }
+}
